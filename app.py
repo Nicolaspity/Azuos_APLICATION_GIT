93a57c7c3ffac7abc6873002ea7f3f06f1bc75a0
Gabriel Toledo Melo <gabriel.t.melo6@aluno.senai.br>
2025-06-29 16:25:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def receber():
 
 if _name_ == "_main_":   
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port,debug=False)
