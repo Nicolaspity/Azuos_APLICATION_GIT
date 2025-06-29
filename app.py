432aec3043e364204c864b6bf88c012bb68e98dc
Mathias-hackerman <mathias.souza@aluno.senai.br>
2025-06-29 12:42:43 -0300

    Mudando a ordem das importações app.py

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify
-from azuos_applied_flow.main import AzuosFlow, ReportState
 import sys
 import os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-
+from azuos_applied_flow.main import AzuosFlow, ReportState
 
 app = Flask(__name__)
 
