4807f56eac29bc0cb0eebb9ce03d08da221dd998
Gabriel Toledo Melo <gabriel.t.melo6@aluno.senai.br>
2025-06-29 17:48:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,6 +25,6 @@ def kickoff():
 def receber():
     return "FOI"
 
-if _name_ == "_main_":   
+if __name__ == "__main__":   
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port,debug=False)
