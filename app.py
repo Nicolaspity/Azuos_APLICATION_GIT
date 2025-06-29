cfb9aa8aff2cea9734ed09df39e6e3cc9fab423c
Gabriel Toledo Melo <gabriel.t.melo6@aluno.senai.br>
2025-06-29 15:50:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,35 +1,29 @@
-from flask import Flask, request, jsonify
 import sys
 import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
-from azuos_applied_flow.main import AzuosFlow, ReportState
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(_file_), 'src')))
+from flask import Flask, request, jsonify
+from azuos_applied_flow.config.config import Config
+from azuos_applied_flow.models.usuario import db
+from azuos_applied_flow.routes.usuario_routes import usuario_bp
+from azuos_applied_flow.services.azuos_service import processar_kickoff
 
-app = Flask(__name__)
+app = Flask(_name_)
+app.config.from_object(Config)
+
+db.init_app(app)
+app.register_blueprint(usuario_bp)
 
 @app.route("/kickoff", methods=["POST"])
 def kickoff():
     data = request.get_json()
     respostas = data.get("respostas", "")
-
     if not respostas:
         return jsonify({"error": "Campo 'respostas' é obrigatório"}), 400
+    return jsonify(processar_kickoff(respostas))
 
-    # Cria estado inicial (opcional, se você quiser pré-popular mais coisas)
-    state = ReportState()
-
-    # Cria Flow
-    flow = AzuosFlow(state=state)
-
-    # Kickoff com inputs (isso dispara TODOS os steps)
-    final_state = flow.kickoff(inputs={"respostas": respostas})
-
-    # Responde estado final
-    return jsonify(final_state.dict())
 @app.route("/receber", methods=["GET"])
 def receber():
-    print('FOI')
     return "FOI"
-if __name__ == "__main__":
-    #app.run(debug=True)
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
